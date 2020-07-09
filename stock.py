4ab32784aa2c5825c6862825090b3aa205fe2a26
João Pedro Chagas <jpchagas@gmail.com>
2020-07-09 00:15:02 -0300

    Add new stocks and add new package

--- stock.py
+++ stock.py
@@ -1,10 +1,20 @@
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as wb
-TickerA='ITSA4.SA'
-TickerB='FLRY3.SA'
-TickerC='LREN3.SA'
+import matplotlib.pyplot as plt
+
+stocks = ['ABEV3.SA','MGLU3.SA','BBAS3.SA','BRKM5.SA','BBDC4.SA','AZUL4.SA','ITUB4.SA','BBDC3.SA','VALE3.SA','PETR4.SA','RENT3.SA','SUZB3.SA','CIEL3.SA','GOLL4.SA','GNDI3.SA','BRAP4.SA','B3SA3.SA','BTOW3.SA','EQTL3.SA']
+
+#TickerA='ITSA4.SA'
+#TickerB='FLRY3.SA'
+#TickerC='LREN3.SA'
 prices=pd.DataFrame()
-tickers = [TickerA, TickerB, TickerC]
-for t in tickers:
-    prices[t]=wb.DataReader(t, data_source='yahoo', start='2010-1-1')['Adj Close']
+#tickers = [TickerA, TickerB, TickerC]
+
+for s in stocks:
+    prices[s]=wb.DataReader(s, data_source='yahoo', start='2020-3-9')['Adj Close']
+
+(prices/prices.iloc[0]*100).plot(figsize=(15,5))
+plt.ylabel('NORMALIZED PRICES')
+plt.xlabel('DATE')
+plt.show()
