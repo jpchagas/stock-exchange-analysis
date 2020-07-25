31fcc890ff3624289c1abc1e93c06977d6d4a51c
João Pedro Chagas <jpchagas@gmail.com>
2020-07-25 19:37:16 -0300

    Start to use CI/CD

--- news.py
+++ news.py
@@ -4,22 +4,22 @@ from newsapi import NewsApiClient
 # Init
 newsapi = NewsApiClient(api_key='376aca59ef1c4bfe9ec2dcc7b9c53231')
 
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us')
-
-# /v2/everything
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
-
+fontes = []
 # /v2/sources
-sources = newsapi.get_sources()
+getsources = newsapi.get_sources(country='br')
+for s in getsources['sources']:
+    fontes.append(s['name'])
+
+#print(fontes)
+
+#/v2/everything
+all_articles = newsapi.get_everything(q='Petrobras',
+                                      sources='Blasting News (BR)','Globo', 'Google News (Brasil)', 'InfoMoney',
+                                      from_param='2020-06-01',
+                                      to='2020-07-01',
+                                      sort_by='relevancy',
+                                      #page=2)
+                                        )
+
+print(all_articles['totalResults'])
+print(all_articles['articles'])
